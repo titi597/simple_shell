25e2cbe731c10b55e1ba6de194fd75ab59c36046
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-08 07:38:41 -0800

    updated corrections

--- exec.c
+++ exec.c
@@ -2,12 +2,19 @@
 
 void execmd(char **argv)
 {
+	pid_t child;
+	int status;
 	char *command = NULL, *actual_command = NULL;
 
 	if (argv && argv[0])
 	{
 		/* get the command */
 		command = argv[0];
+
+		if (strcmp(command, "exit") == 0)
+		{
+			exit(0);
+		}
 
 		/* generate the path to this command before passing it to execve */
 		actual_command = get_location(command);
@@ -17,11 +24,23 @@ void execmd(char **argv)
 			perror("./hsh");
 			return;
 		}
-		/* This code will be executed in the child process. */
-		if (execve(actual_command, argv, NULL) == -1)
+		child = fork();
+
+		if (child == 0)
 		{
-			perror("Error");
-			exit(EXIT_FAILURE);
+			/* This code will be executed in the child process. */
+			if (execve(actual_command, argv, NULL) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+
+			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+				return;
 		}
 	}
 }
