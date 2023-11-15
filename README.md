53b45d6779e97aa549a8765e2dc1e35286ae394e
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-15 03:18:40 -0800

    shell

--- README.md
+++
@@ -1,1 +0,0 @@
-This our readme file on simple_shell project that is done by Schiphtan and Thierry
--- env_command.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENV_COMMAND_H
-#define ENV_COMMAND_H
-
-int execute_setenv(char **tokens);
-int execute_unsetenv(char **tokens);
-
-#endif
