64009598f42b430075a4dc150457cdbd789c943e
Staner <tuyumvire@gmail.com>
2023-11-15 20:16:29 -0800

    changed commands_handler.c

--- commands_handler.c
+++ commands_handler.c
@@ -1,38 +1,45 @@
 #include "shell.h"
+#include "split_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_PATH_LENGTH 1024
 
 /**
  * handle_commands - function that handles commands separated by ';'
  * @line: input line containing commands
- */
+*/
+
 void handle_commands(char *line)
 {
-    char *token, *saveptr;
-    char *commands[256];  // Adjust the size as needed
+        char *token, *saveptr;
+        char *commands[256]; /*Size is adjustable*/
 
-    // Tokenize the input line based on ';'
-    int i = 0;
-    token = strtok_r(line, ";", &saveptr);
-    while (token != NULL)
-    {
-        commands[i++] = token;
-        token = strtok_r(NULL, ";", &saveptr);
-    }
-    commands[i] = NULL;
+        /*Tokenize the input line base on ';' */
+        int i = 0;
 
-    // Execute each command
-    i = 0;
-    while (commands[i] != NULL)
-    {
-        char *command_line = commands[i];
-        char **argv = split_line(command_line);
+        token = strtok_r(line, ";", &saveptr);
 
-        if (argv != NULL && argv[0] != NULL)
-        {
-            execmd(argv);
-            free(argv);
+        while (token != NULL)
+        {commands[i++] = token;
+                token = strtok_r(NULL, ";", &saveptr);
         }
+        commands[i] = NULL;
+        /*Execute each command*/
+        i = 0;
+        while (commands[i] != NULL)
+        {
+                char *command_line = commands[i];
+                /* Split the command based on spaces*/
+                char **argv = split_line(command_line);
 
-        i++;
-    }
+                if (argv != NULL && argv[0] != NULL)
+                {
+                        execmd(argv);
+                        free(argv);
+                }
+                i++;
+        }
 }
-
